145c893d016fd90eb00a969096612c1b3c16873c
Majey <kevinerick78@yahoo.com>
2023-05-04 21:34:22 +0300

    Navbar

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,19 @@
+import "./topbar.scss"
+
+export default function Topbar() {
+  return (
+    <div className='topbar' id="topbar">
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro">STER</a>
+        </div>
+
+        <h1>HELOO</h1>
+
+        <div className="right">
+          THIS SIS RIGHT
+        </div>
+      </div>
+    </div>
+  )
+}
