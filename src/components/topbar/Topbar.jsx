0c3fc2150face115e46fc22bcd11bf6b789e9d56
Majey <kevinerick78@yahoo.com>
2023-05-08 22:50:36 +0300

    @material-ui/icons

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,13 +1,21 @@
 import "./topbar.scss"
+import {Person, Mail} from "@material-ui/icons"
 
 export default function Topbar() {
   return (
     <div className='topbar' id="topbar">
       <div className="wrapper">
         <div className="left">
-          <a href="#intro" className="logo">MAJEY</a>
+          <a href="#intro" className="logo">MAJEY.</a>
+
           <div className="itemContainer">
-            
+            <Person className="icon"/>
+            <span>+254 713 849 900</span>            
+          </div>
+
+          <div className="itemContainer">
+            <Mail className="icon"/>
+            <span>kevinerick78@yahoo.com</span>            
           </div>
         </div>
 
