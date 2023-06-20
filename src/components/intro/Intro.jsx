787cd5120b27470addecb6024c6b8d1f4f03a962
Majey <kevinerick78@yahoo.com>
2023-06-20 18:51:25 +0300

    text typing animation

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,39 @@
+import { useEffect, useRef } from "react"
 import "./intro.scss"
+import { init } from 'ityped';
 
 export default function Intro() {
+  const textRef = useRef(null);
+
+  useEffect(()=>{
+    init(textRef.current,{
+      showCursor: true,
+      backDelay: 1500,
+      backSpeed:60,      
+      strings: ["Full-Stack Developer","Designer","SEO Expert", "Digital Marketer"],
+    });
+
+  },[]);
+
   return (
     <div className="intro" id="intro">
-      Intro
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/man.png" alt="" />
+        </div>
+      </div>
+
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There, I'm</h2>
+          <h1>Kevin Majengo</h1>
+          <h3>Freelance <span ref={textRef}></span></h3>
+        </div>
+
+        <a href="#portfolio">
+          <img src="assets/down.png" alt="" />
+        </a>
+      </div>
     </div>
   )
 }
